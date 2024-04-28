dc6a0e94ccb0017df78ffea6b60fc94778d9332b
younesgrf <you@example.com>
2024-04-28 02:47:31 +0100

    last updates

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'tablewidget.dart';
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,92 +7,163 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text('Home Page'),
-    Text('About Us Page'),
-    Text('Contact Us Page'),
-    TableWidget(articles: [
-      {
-        'id': 1,
-        'name': 'yahia slimani',
-        'title': 'djamal belmadi',
-        'desc':
-        'Djamal belmadi est un entrainer de l\'equipe natinal de l\'algerie , il est gagné la coup d\'afrique en 2019....',
-        'cat': 'sport'
-      },
-      {
-        'id': 2,
-        'name': 'yahia slimani',
-        'title': 'djamal belmadi',
-        'desc':
-        'Djamal belmadi est un entrainer de l\'equipe natinal de l\'algerie , il est gagné la coup d\'afrique en 2019....',
-        'cat': 'sport'
-      },
-      {
-        'id': 3,
-        'name': 'yahia slimani',
-        'title': 'djamal belmadi',
-        'desc':
-        'Djamal belmadi est un entrainer de l\'equipe natinal de l\'algerie , il est gagné la coup d\'afrique en 2019....',
-        'cat': 'sport'
-      },
-    ]),
+  List imgData = [
+    "images/about.png",
+    "images/job.png",
+    "images/mcqs.png",
+    "images/pastpapers.png",
+    "images/pdf.png",
+    "images/quiz.png",
   ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+  List titles =[
+    "ABOUT",
+    "JOB",
+    "MCQS",
+    "PAPERS",
+    "PDF",
+    "QUIZ",
+  ];
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'ThemaArabia',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.green),
+        body: SingleChildScrollView(
+          child: Container(
+            color: Colors.green,
+            width: width,
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                  ),
+                  height: height * 0.23,
+                  width: width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(
+                      top: 35,
+                      left: 20,
+                      right: 20,
+          
+                   ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                          InkWell(
+                            onTap: (){},
+                            child: Icon(Icons.sort,
+                              color: Colors.white,
+                              size: 40,
+                            ),
+                          ),
+                            Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: Colors.white,
+                                image: DecorationImage(
+                                  image: AssetImage("images/woman.png")
+                                ),
+                              ),
+                            ),
+                        ],
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(
+                        top: 20,
+                        left: 30,
+                      ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Dashboard",
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: 1,
+                            ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              "Last Update 28 april 2024",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white54,
+                                letterSpacing: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ) ,
+                ),
+                SingleChildScrollView(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
+                    ),
+                   // height: height ,
+                    width: width,
+                    padding: EdgeInsets.only(bottom: 20),
+                    //padding: EdgeInsets.symmetric(vertical: 50),
+                    child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 1.1,
+                        mainAxisSpacing: 25,
+                      ),
+                    shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: imgData.length,
+                      itemBuilder: (context, index){
+                        return InkWell(
+                          onTap: (){},
+                          child: Container(
+                            margin: EdgeInsets.symmetric(vertical: 8,horizontal: 20),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white,
+                              boxShadow: [BoxShadow(
+                                color: Colors.black26,
+                                spreadRadius: 1,
+                                blurRadius: 6,
+                              ),
+                              ],
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly ,
+                              children: [
+                                Image.asset(imgData[index],
+                                width: 100,
+                                ),
+                                Text(titles[index],
+                                style:TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ) ,
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ) ,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () {
-              // Action à effectuer lors du clic sur le bouton de déconnexion
-              Navigator.pushReplacementNamed(context, '/login');
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info),
-            label: 'About Us',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.contacts),
-            label: 'Contact Us',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.article),
-            label: 'My Articles',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.black,
-        selectedIconTheme: IconThemeData(color: Colors.black),
-        onTap: _onItemTapped,
-      ),
     );
   }
 }
